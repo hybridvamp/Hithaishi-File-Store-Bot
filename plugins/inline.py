b82eabb74ffe4e8231286937b0a1fdc31c39a4e0
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2022-02-19 03:01:57 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,7 +26,7 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='😂 Search Your Own Files BRO 😂a',
+                           switch_pm_text='🥲 USE Group Bro This for Admin Only 🤠',
                            switch_pm_parameter="hehe")
         return
 
