037193a5cc551688688b75b2cb478ba57497dac5
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2022-06-08 08:11:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.reply_sticker("CAACAgUAAxkBAAED9-piELvK2mw40iShaFUktSKe-jgOfQAClgMAAgQuiFSgnaE2_ZABNCME")
+            k = await query.message.reply_sticker("CAACAgUAAxkBAAEBIAhioAqcNTZN_eP18yhFl35akFw0swACEwUAAl6BAVWSdOPhVKk_6SQE")
             await asyncio.sleep(10)
             await k.delete()
 
