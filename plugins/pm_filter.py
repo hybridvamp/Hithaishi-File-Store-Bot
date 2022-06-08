1208c6ace272185e58ca6456e46d0d0e21191006
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2022-06-08 12:47:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -779,7 +779,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    s = await msg.reply_sticker("CAACAgUAAxkBAAED9-hiELXYEnJKqYDHyjr88gwI0nquPgACCgYAAjCWgVTry1pKvQWG0iME",
+    s = await msg.reply_sticker("CAACAgUAAxkBAAEE70VioArdD5I6Grlsm4HUPKz4hZxYrAACEwUAAl6BAVWSdOPhVKk_6SQE",
                     reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(10)
     await s.delete()
