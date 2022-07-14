2e62d8230f3d7a8bb4d554f4a30755d12a4b0f76
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2022-07-14 18:32:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -364,7 +364,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 mention = f"{query.from_user.mention}"
 
             try:
-                   if AUTH_CHANNEL and not await is_subscribed(client, query):
+                if AUTH_CHANNEL and not await is_subscribed(client, query):
                     await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                     return
                 elif settings['botpm']:
