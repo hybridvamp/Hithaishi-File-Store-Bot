2a71e03461e38f7e3edb140af47b0e78444dd400
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2022-10-04 11:39:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -712,7 +712,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"💿 <b>𝗤𝗨𝗘𝗥𝗬 :</b>  <code>{search}</code>"
+        cap = f"💿 <b>𝗤𝗨𝗘𝗥𝗬 :</b>  <code>{search}</code> \n\n[🎃 𝗝𝗢𝗜𝗡 𝗙𝗢𝗥 𝗠𝗢𝗩𝗜𝗘𝗦 🎃](https://t.me/Hithaishi_Desai)\n\n[⚠️ 𝗥𝗘𝗔𝗗 𝗥𝗨𝗟𝗘𝗦 ⚠️](https://t.me/HITHAISHI_DESAI_CHAT/77321) "
     if imdb and imdb.get('poster'):
         try:
            a = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
